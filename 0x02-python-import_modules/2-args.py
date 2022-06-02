e2258e4518de2c14fdbd5dbe0956d765ba3ce3a3
PROFAY <Oyewaletimmy01@gmail.com>
2022-06-02 13:04:39 +0000

    correcting ouput

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ import sys
 def main():
     args = len(sys.argv) - 1
     if args == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif args == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
