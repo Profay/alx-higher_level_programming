3c409e89a9d550a6b41a0aedb86fb56b4334713e
PROFAY <Oyewaletimmy01@gmail.com>
2022-06-04 22:17:15 +0000

    test

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     a = len(my_list) - 1
     if idx >= 0 and idx <= a:
         my_list[idx] = element
