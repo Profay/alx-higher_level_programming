0a20f1304247c23285aa9c171c556ece6406c81c
PROFAY <Oyewaletimmy01@gmail.com>
2022-06-05 11:58:14 +0000

    multiple return

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence != "":
+        a = len(sentence)
+        b = sentence[0]
+        c = [a , b]
+        return c
+    else:
+        a = len(sentence)
+        b = None
+        c = [a , b]
+        return c
