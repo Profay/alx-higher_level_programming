c863f87382bc59cdaaed751d732b312fd354da67
PROFAY <Oyewaletimmy01@gmail.com>
2022-06-06 06:26:59 +0000

    correcting loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    for a in range(0, len(my_list)):
-        if idx != a:
-            return my_list
+    a = len(my_list) - 1
+    if idx >= 0 and idx <= a:
         del my_list[idx]
+        return my_list
+    else:
         return my_list
