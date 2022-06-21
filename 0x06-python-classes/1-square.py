725b72d685688da0a3703f60919a5a50eb1de059
profay <oyewaletimmy01@gmail.com>
2022-06-21 14:23:30 +0000

    creating size in class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+""" Module Sqaure """
+
 
 class Square:
-
+    """ Definition of a Class Square
+        Attributes :
+                  size (int): Size of square
+    """
     def __init__(self, size):
-            self.__size = size
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        """
+        self.__size = size
 
