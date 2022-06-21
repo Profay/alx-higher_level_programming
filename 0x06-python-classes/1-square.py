dd840f443846567104d20a4851135b175c379c55
profay <oyewaletimmy01@gmail.com>
2022-06-21 14:21:23 +0000

    creating size in class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    _Square__size = None
-    def __init__(self, size=None):
-        if size is not None:
-            self._Square__size = size
+
+    def __init__(self, size):
+            self.__size = size
 
