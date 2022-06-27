1b108e0073f6a586b82917cb4eb49bee3296e32f
profay <oyewaletimmy01@gmail.com>
2022-06-27 11:29:28 +0000

    rectifying

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         """
         Sets the value of the width
         """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -49,7 +49,7 @@ class Rectangle:
         """
         sets the value of height
         """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
