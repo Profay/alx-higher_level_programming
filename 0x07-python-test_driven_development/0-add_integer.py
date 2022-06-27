9bda9150992cc3c748bbb9a17538a104293955b2
profay <oyewaletimmy01@gmail.com>
2022-06-27 20:26:33 +0000

    test driven file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
 Modules for adding two integer together
+Two parameters are passed and the sum is returned if value.
+If nothing is passed for b, b assumed a default value of 89.
 """
 
 
 def add_integer(a, b=98):
     """
-    taking two integers and returning the sum
+    Taking two integers and returning the sum
+    b is 89 by default
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
