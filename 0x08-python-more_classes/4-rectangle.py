16dfcff2b0744adb0457f6824a48a284f9419766
profay <oyewaletimmy01@gmail.com>
2022-06-27 13:10:34 +0000

    pycodestyle checker

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -81,6 +81,7 @@ class Rectangle:
                 if i < self.__height - 1:
                     print_string += "\n"
             return print_string
+
     def __repr__(self):
         """
         Instance Method : repr
