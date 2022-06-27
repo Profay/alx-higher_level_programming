a2dc3845b17e4263ccd3773578a0bbd0f456d92b
profay <oyewaletimmy01@gmail.com>
2022-06-27 08:07:50 +0000

    Reademe file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """
+    Declaring a class for rectangle
+    Rectangle will have its width and height
+    Cases of Area might be included later on.
+    """
+    pass
