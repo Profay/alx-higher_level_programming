e9a81b02b228f626db57aa5b7ab526257332cfed
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-05 13:27:37 +0000

    Pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,9 +5,9 @@ Modules for json string representation
 import json
 
 
-
 def to_json_string(my_obj):
     """
-    This function return the json string representation of the argument recieved
+    This function return the json string
+    representation of the argument recieved
     """
     return json.dumps(my_obj)
