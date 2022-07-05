59f30b1697db4b1059c945ea7573303f18c45e5b
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-05 20:26:14 +0000

    making file exexutable

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ to a Python list, and then save them to a file
 
 
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    my_list = load_from_json_file("add_item.json")
+    my_list = load("add_item.json")
 except FileNotFoundError:
     my_list = []
-for arg in argv[1:]:
-    my_list.append(arg)
-save_to_json_file(my_list, "add_item.json")
+for argc in argv[1:]:
+    my_list.append(argc)
+save(my_list, "add_item.json")
