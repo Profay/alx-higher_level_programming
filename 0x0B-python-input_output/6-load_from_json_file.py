15a679b3142a3b4b79946ec3da58d0dadc8cb0d3
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-05 20:15:49 +0000

    updates

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """ The function writes an Object to a text file,
     using a JSON representation """
     with open(filename, "r", encoding='utf-8') as file:
-        return (json.loads(file.read))
+        return (json.loads(file.read()))
