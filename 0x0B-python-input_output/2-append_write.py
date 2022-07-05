09d0153d9affd4141fdad068e1ec664c0eb3b562
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-05 13:11:44 +0000

    Appending text to an open file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Modules for appending file [with]
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Function for appending new contents to a file
+    Returning the lenght of the text
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(letters)
