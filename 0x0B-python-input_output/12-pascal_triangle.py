a94feb576a9f2eba2ce72f11a014955dca137210
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-05 20:51:44 +0000

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,3 +9,15 @@ def pascal_triangle(n):
     representing the Pascal's triangle of n"""
     if n <= 0:
         return []
+    if n == 1:
+        return [[1]]
+    triangle = []
+    for i in range(n):
+        row = [0] * (i + 1)
+        triangle.append(row)
+    triangle[0][0] = 1
+    for row in range(n - 1):
+        for col in range(len(triangle[row])):
+            triangle[row + 1][col] += triangle[row][col]
+            triangle[row + 1][col + 1] += triangle[row][col]
+    return triangle
