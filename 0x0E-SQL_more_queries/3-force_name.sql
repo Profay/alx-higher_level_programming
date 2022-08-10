569f6f7e68db589c273b8294937bd636e335356e
PROFAY <Oyewaletimmy01@gmail.com>
2022-08-10 12:38:01 +0000

    kk

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- script that creates a table called force_table in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- script that creates a table called id_not_null in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256)
+);
