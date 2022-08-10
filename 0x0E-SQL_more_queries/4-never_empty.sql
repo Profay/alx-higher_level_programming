1769e6118ee5dad610a970cc85585f0b773d71cf
PROFAY <85462029+Profay@users.noreply.github.com>
2022-08-10 23:46:19 +0100

    Update 4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- script that creates a table called id_not_null in the current database in MySQL server
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
 );
