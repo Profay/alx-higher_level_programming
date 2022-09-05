0e3dec5c700a00048f5c47908fd96c77d4b83721
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-05 16:08:53 +0000

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,8 @@
 const argv = process.argv;
 if (argv.length === 2) {
   console.log('No argument');
-}
-else if (argv.length === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments Found');
 }
