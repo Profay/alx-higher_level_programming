5f0287208d4a1fe66403926518ff20a7a804c406
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-06 17:01:26 +0000

    occurecnce

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
+module.exports.nbOccurences = function (list, searchElement) {
   let a = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i].id === searchElement) {
-      a++;
-      return a;
+    if (list[i] === searchElement) {
+      a += 1; 
     }
   }
+  return a;
 };
