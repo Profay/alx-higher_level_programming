efa96bad4cabcb4631eece2c775a0ab2b5221f78
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-10 11:09:21 +0000

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module uses MySQLdb to print out states
+
+Usage:
+    ./filename user pw db
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    args = sys.argv
+    dt = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[1],
+        passwd=args[2],
+        db=args[3],
+        charset="utf8")
+    newName = args[4]
+    cursor = dt.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s\
+                   ORDER BY states.id", (newName,))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == newName:
+            print(row)
+    cursor.close()
+    dt.close()
