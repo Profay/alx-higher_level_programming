5ffc187d788d5f006d83fa92ed2cab5a28c8ec1a
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-11 16:16:06 +0000

    pycodestyle

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 
 
@@ -13,9 +13,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
         sys.argv[2],
-        sys.argv[3]), 
-        pool_pre_ping=True
-)
+        sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
