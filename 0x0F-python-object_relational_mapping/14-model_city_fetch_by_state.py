666947bdb0eae180471f5bda702a8b2a29521f98
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-11 18:44:13 +0000

    add scity object

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
 
     session = Session(engine)
     for city, state in session.query(City, State).\
-        join(State).order_by(City.id):
+            join(State).order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
