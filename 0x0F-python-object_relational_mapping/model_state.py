6f76a97c52263c58a8343c76cb55341296d62d4b
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-11 15:49:26 +0000

    ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ The class inherits from declarative_base. """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
