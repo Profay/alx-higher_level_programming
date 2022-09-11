778339b439482d2ad1f751f5ba88ce0498b77d65
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-11 17:11:57 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    newState = session.add(State(name='Louisiana'))
+    session.commit()
+    rows = session.query(State).order_by(State.id)
+    for row in rows:
+        if row.name == 'Louisiana':
+            print("{}".format(row.id))
+    session.close()
