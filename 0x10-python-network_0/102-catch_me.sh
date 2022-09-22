94fcfbeb943da479115076796770f3c2510d04cb
PROFAY <85462029+Profay@users.noreply.github.com>
2022-09-22 21:04:20 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get through to secret domain
-curl -H 'Origin: HolbertonSchool' -sLX PUT -d user_id=98 "$1"
+# Script that makes a request to 0.0.0.0:5000/catch_me and get the response
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
