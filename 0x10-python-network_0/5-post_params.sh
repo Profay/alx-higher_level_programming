e14c91ff02fa8809be9f2283b2f9c216f373192e
PROFAY <85462029+Profay@users.noreply.github.com>
2022-09-22 20:47:28 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Post some data to a web server
-curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
+curl -sd 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
