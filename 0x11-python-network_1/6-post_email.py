b8f30e9130266c5578b6c9ea18956551450d863f
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-23 16:52:48 +0000

    ggg

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+import requests
+import sys
+
+
+def get_POST_mail():
+    """ This function retuens an email using
+    POST request.
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    response = requests.post(url, email=email)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    get_POST_mail()
