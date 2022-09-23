56d7278d542986eb50182475ccfc6bc6d554ea19
PROFAY <Oyewaletimmy01@gmail.com>
2022-09-23 17:05:11 +0000

    update post email

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-""" This module takes in a URL and print error
-status code.
-"""
-
+""" Fetches https://intranet.hbtn.io/status """
 import requests
-import sys
-
-
-def error_status():
-    """ This function prints an error status code.
-    """
-    url = sys.argv[1]
-    response = requests.get(url)
-    if requests.status_codes > 399:
-        print('Error code: {}'.format(request.status_code))
-
+from sys import argv
 
 if __name__ == "__main__":
-    error_status()
+    request = requests.get(argv[1])
+
+    if request.status_code < 400:
+        print(request.text)
+    else:
+        print('Error code: {}'.format(request.status_code))
