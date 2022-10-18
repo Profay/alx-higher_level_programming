5f5cc3f2c9b01d359384b89a3f3c77b6329287e2
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-18 19:37:40 +0000

    get url and print status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+const request = require('request');
+request.get(process.argv[2], function (response) {
+  // if (error) {
+  //    console.log('code: ' + error.code);
+  // }
+  if (response) {
+    console.log('code: ' + response.statusCode);
+  }
+});
