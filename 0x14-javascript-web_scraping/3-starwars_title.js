7c4fb49e1608d998d84c303b4ca3574d93006100
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-18 20:32:47 +0000

    good

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+const request = require('request');
+request.get(('https://swapi-api.hbtn.io/api/films/' + process.argv[2]), function (error, response, body) {
+  if (error == null) {
+    const movies = JSON.parse(body);
+    console.log(movies.title);
+  }
+});
