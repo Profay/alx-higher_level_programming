48ea6315f144a75624f252937648d87ce1e15c54
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-18 21:32:07 +0000

    characters in starwar movie sorted

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+const request = require('request');
+request.get(('https://swapi-api.hbtn.io/api/films/' + process.argv[2]), async function (error, response, body) {
+  if (error == null) {
+    const movies = JSON.parse(body);
+    for (let i = 0; i < movies.characters.length; i++) {
+      await new Promise(function (resolve, reject) {
+        request(movies.characters[i], function (error, respose, body) {
+          if (error == null) {
+            const character = JSON.parse(body);
+            console.log(character.name);
+            resolve();
+          }
+        });
+      });
+    }
+  }
+});
