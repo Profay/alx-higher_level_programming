9392e80a3b75b5ebaef255e793d2a825bca4f85b
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-18 19:05:22 +0000

    read a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+const fs = require('fs');
+const path = require('process.argv[2]');
+fs.readFile(path, 'utf8', function (error, data) {
+  console.log(error || data);
+});
